b1edc2b45d85a598b9aefae5135da6ebd274bc31
vkarthikgourav <vkg.karthik.gourav@gmail.com>
2026-03-09 11:01:58 +0530

    Final commit

--- core/security.py
+++ core/security.py
@@ -1,10 +1,16 @@
 from jose import jwt 
-
 from datetime import datetime, timedelta
+import os 
+from dotenv import load_dotenv
 
-SECRET_KEY="S#e#c#r#e#t"
-ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES=60
+
+# SECRET_KEY="S#e#c#r#e#t"#os.load_dotenv()
+# ALGORITHM="HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES=60
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 
 def create_access_token(data:dict):
     to_encode=data.copy()
